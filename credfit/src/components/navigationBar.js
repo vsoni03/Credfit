797f02caa8c9e2bc00221aca2ff4dff840a39861
Vruti Soni <vrutisoni@Vrutis-MacBook-Pro-2.local>
2023-04-08 22:54:32 -0400

    made the heading and the logo

--- credfit/src/components/navigationBar.js
+++ credfit/src/components/navigationBar.js
@@ -1,9 +1,14 @@
-// import logo from './logo.svg';
+import navlogo from './credfitLogo.png';
+import caplogo from './capitalOne.png'
 import '../components/navigationBar.css';
 
 function navigationBar() {
     return(
-        <div class = "navbar"></div>
+        <div class = "navbar"> 
+         <img src={navlogo} className="navlogo" alt="logo"/>
+         <img src={caplogo} className="caplogo" alt="logo"/>
+        </div>
+
     )
 }
 export default navigationBar;
