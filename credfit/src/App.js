ce92acba9433fbde143aea0b9c8c3607c422b320
Vruti Soni <vrutisoni@Vrutis-MacBook-Pro-2.local>
2023-04-08 21:45:21 -0400

    added the navigation bar didnt spell this wrong :)

--- credfit/src/App.js
+++ credfit/src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import NavigationBar from './components/navigationBar.js'
+
 
 function App() {
   return (
     <div className="App">
+      <NavigationBar/>
       <header className="App-header">
+       
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ credfit/src/components/navigationBar.js
@@ -0,0 +1,10 @@
+// import logo from './logo.svg';
+import '../components/navigationBar.css';
+
+function navigationBar() {
+    return(
+        <div class = "navbar"></div>
+    )
+}
+export default navigationBar;
+
